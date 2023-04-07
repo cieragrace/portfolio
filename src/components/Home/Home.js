c1a3fc37203480cd5ef405a12d417bd8228b84c6
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-07 16:45:52 -0600

    Add styling to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,20 +6,24 @@ const Home = () => {
       <nav className='nav-container'>
         <h3>Ciera Muniz FEE</h3>
         <div className='nav-link-container'>
-          <button className='about-link'>About |</button>
-          <button className='projects-link'> Projects |</button>
-          <button className='contact-link'>Contact</button>
+          <button className='about link'>About</button>
+          <div>|</div>
+          <button className='projects link'>Projects</button>
+          <div>|</div>
+          <button className='contact link'>Contact</button>
         </div>
       </nav>
       <main className='main-container'>
         <section className='info-section'>
-          <h1 className='hello'>Hello,</h1>
-          <h3 className='intro'>A bit about me</h3>
-          <p className='blurb'></p>
+          <div className='info-text-container'>
+            <h1 className='hello'>Hello there,</h1>
+            <h3 className='intro'>A bit about me</h3>
+            <p className='blurb'>A Front End developer with over 10 years experience in restaurant management. Passionate and focused on accessibility, empathy and inclusivity in my work. An efficient learner with the ability to adapt quickly in a new environment individually or when working on a team. Dedicated hard worker, ready to help make big changes for people in a positive way.</p>
+          </div>
           <div className='circle-link-container'>
-            <button className='about circle'></button>
-            <button className='projects circle'></button>
-            <button className='contact circle'></button>
+            <button className='about circle'>About</button>
+            <button className='projects circle'>Projects</button>
+            <button className='contact circle'>Contact</button>
           </div>
         </section>
         <section className='image-section'>
@@ -27,9 +31,9 @@ const Home = () => {
         </section>
       </main>
       <footer className='footer-container'>
-        <button className='resume'>resume</button>
-        <button className='linkedIn'>linkedIn</button>
-        <button className='github'>GitHub</button>
+        <button className='resume link'>resume</button>
+        <button className='linkedIn link'>linkedIn</button>
+        <button className='github link'>GitHub</button>
       </footer>
     </div>
   )
