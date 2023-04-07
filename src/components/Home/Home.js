b192d657109a5a0b56f360c655030657d50bb51c
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-07 17:43:02 -0600

    Add home media queries

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,9 +16,9 @@ const Home = () => {
       <main className='main-container'>
         <section className='info-section'>
           <div className='info-text-container'>
-            <h1 className='hello'>Hello there,</h1>
-            <h3 className='intro'>A bit about me</h3>
-            <p className='blurb'>A Front End developer with over 10 years experience in restaurant management. Passionate and focused on accessibility, empathy and inclusivity in my work. An efficient learner with the ability to adapt quickly in a new environment individually or when working on a team. Dedicated hard worker, ready to help make big changes for people in a positive way.</p>
+            <h1 className='hello'>Hey there,</h1>
+            <h3 className='intro'>Here's a bit about me...</h3>
+            <p className='blurb'>I am a Denver based Front End developer with over 10 years experience in restaurant management. I am passionate and focused on accessibility, empathy and inclusivity in my work. In my time at Turing I continued to prove that I am an efficient learner with the ability to adapt quickly in a new environment individually or when working on a team. I am a dedicated hard worker, and am here to help make big changes for people in a positive way.</p>
           </div>
           <div className='circle-link-container'>
             <button className='about circle'>About</button>
