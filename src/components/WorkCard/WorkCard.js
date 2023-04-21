8066620e6762586e26a4ab7798cd2e8ff93a1176
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-21 16:46:04 -0600

    Add props to work and workcard

--- src/components/WorkCard/WorkCard.js
+++ src/components/WorkCard/WorkCard.js
@@ -2,7 +2,11 @@ import './WordCard.css'
 
 const WorkCard = () => {
   return(
-    <></>
+    <div className='workcard-container'>
+      <div className='card-image'></div>
+      <p className='type'></p>
+      <p className='summary'></p>
+    </div>
   )
 }
 
