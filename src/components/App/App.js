2fabd2cd01c0a3c24447a3f9e419181a04c12fa6
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-04-21 15:49:43 -0600

    Add imports and Routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,45 @@
 
 import './App.css';
 import Home from '../Home/Home'
+import About from '../About/About';
+import Work from '../Work/Work'
+import Contact from '../Contact/Contact';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='app-container'>
-      <Home />
+      <Switch>
+        <Route 
+          exact
+          path='/'
+          render={() => (
+            <Home />
+          )}
+        />
+        <Route
+          exact
+          path='/about'
+          render={() => (
+            <About />
+          )}
+        />
+        <Route
+          exact
+          path='/projects'
+          render={() => (
+            <Work />
+          )}
+        />
+        <Route
+        exact
+        path='/contact'
+        render={() => (
+          <Contact />
+        )}>
+
+        </Route>
+      </Switch>
     </div>
   );
 }
